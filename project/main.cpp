14bd46ce09fb2c8e22aaf2079cfbb261c164ffd2
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-30 17:00:15 -0400

    Setting up file structure

---
+++ project/main.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include "math_functions.h"
--- project/math_functions.h
+++ project/math_functions.h
@@ -1,1 +1,2 @@
 //math_functions.h is included in in the main.cpp file
+//I will declare the functions from the provided math_functions.cpp file
