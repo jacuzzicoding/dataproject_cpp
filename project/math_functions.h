74dbb2c4f1a08160a7cbfdf8828636266c6f957d
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-30 17:13:15 -0400

    Add math_functions.h header file

--- project/math_functions.h
+++ project/math_functions.h
@@ -1,2 +1,25 @@
 //math_functions.h is included in in the main.cpp file
 //I will declare the functions from the provided math_functions.cpp file
+
+#ifndef math_functions.h
+#define math_functions.h
+
+double arithmeticMean(const std::vector<int>& numbers);
+
+double geometricMean(const std::vector<int>& numbers);
+
+double harmonicMean(const std::vector<int>& numbers);
+
+double standardDeviation(const std::vector<int>& numbers);
+
+double variance(const std::vector<int>& numbers);
+
+double median(std::vector<int> numbers);
+
+int mode(const std::vector<int>& numbers);
+
+int maximumNumber(const std::vector<int>& numbers);
+
+int minimumNumber(const std::vector<int>& numbers);
+
+#endif
